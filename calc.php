fb6a5f9fbdf528fc6787d79c70752005d4ffa9ca
insanexxx <isenov420@gmail.com>
2021-10-04 15:27:59 +0500

    master

---
+++ calc.php
@@ -0,0 +1,9 @@
+<?php
+$startDate = $_POST["startDate"];
+$sum = $_POST["sum"];
+$term = $_POST["term"];
+$percent = $_POST["percent"];
+$sumAdd = $_POST["sumAdd"];
+
+echo "Ваше имя: <b>".$name . " " . $surname . "</b>";
+?>
---
+++ script.js
@@ -0,0 +1,60 @@
+$.datepicker.regional['ru'] = {
+	closeText: 'Закрыть',
+	prevText: 'Предыдущий',
+	nextText: 'Следующий',
+	currentText: 'Сегодня',
+	monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+	monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
+	dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+	dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+	dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+	weekHeader: 'Не',
+	dateFormat: 'dd.mm.yy',
+	firstDay: 1,
+	isRTL: false,
+	showMonthAfterYear: false,
+	yearSuffix: ''
+};
+$.datepicker.setDefaults($.datepicker.regional['ru']);
+
+$( function() {
+    $.datepicker.setDefaults($.datepicker.regional['ru']);
+    $('#datepicker').datepicker({
+	    showOn: "both"
+	});
+  } );
+
+  $(document).ready(function() {
+    $("#mainform").validate({
+        rules: {
+            sum: {
+                required: true,
+                minlenght: 4,
+                maxlenght: 7,
+                min: 1000,
+                max: 3000000
+            },
+            term: {
+                required: true,
+                minlenght: 1,
+                maxlenght: 2,
+                min: 1,
+                max: 60
+            },
+            sumAdd: {
+                required: true,
+                minlenght: 1,
+                maxlenght: 7,
+                min: 0,
+                max: 3000000
+            },
+            percent:{
+                required: true,
+                minlenght: 1,
+                maxlenght: 3,
+                min: 3,
+                max: 100
+            }
+        }
+    });
+    });
